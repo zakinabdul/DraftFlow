c289ed600952a550672c229dc3a854e58bd45972
zakinabdul <zakinabdul.tech@gmail.com>
2025-12-30 16:33:04 +0000

    Add CORS middleware to FastAPI application for cross-origin requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.api.v2 import api_router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 @app.get("/")
@@ -8,6 +9,12 @@ def index_mesg():
         "message": "Use /docs endpoint for now"
     }
     
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(api_router, prefix="/api/v2")
